e8e28293548e0fd085cf97ef38d6c9a5cc927c50
emmanuelmacharia <samuelmarsha@outlook.com>
2019-10-24 14:27:03 +0300

    completed default navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 
 const appRoutes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductPageComponent }
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
