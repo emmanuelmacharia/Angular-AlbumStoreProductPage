60188927d050cac42bd5458d67d749e4bec9eacc
emmanuelmacharia <samuelmarsha@outlook.com>
2019-10-24 14:30:34 +0300

    completed default navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-const appRoutes = [
+const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductPageComponent },
   { path: '', redirectTo: 'products', pathMatch: 'full' }
